0fbe637f2d8fd330467b06f3ed7316a5c196f1a7
전민기 <firesped1@gmail.com>
2020-01-04 13:28:07 +0900

    Heroku용 몇몇개 수정
     - package.json에 nodejs, npm 버전 추가
     - index.js의 임시 포트(3000)을 heroku용 포트로 수정

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const port = process.env.PORT;
+
 app.get('/', (req,res) => res.sendFile(__dirname + '/index.html'));
 app.use('/static', express.static(__dirname + '/public'));
 
@@ -35,4 +37,4 @@ io.on('connection', function(socket){
 
 })
 
-http.listen(3000, () => console.log('listening on *:3000'));
+http.listen(port, () => console.log('listening on *:3000'));
